0968790b3c2f5b1542360660c388822141d0d637
elvisonobo <rapknowlogy@gmail.com>
2020-02-26 08:40:49 +0100

    created and used middleware on server and maintennace

--- server.js
+++ server.js
@@ -1,13 +1,33 @@
 // import express
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 var app = express();
 
 hbs.registerPartials(__dirname+'/views/partials')
 // register templating engine
 app.set('view engine', 'hbs');
+
+// using a middleware
+app.use((req, res, next)=>{
+    var now = new Date().toString();
+    var log = `${now}: ${req.method}, ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err)=>{
+        if(err){
+            console.log('unable to append to server.log');
+        }
+    })
+    next();
+});
+
+// app.use((req,res,next)=>{
+//     res.render('maintenance.hbs');
+// });
+
 // serve static file
 app.use(express.static(__dirname+'/public'))
+
 hbs.registerHelper('getCurrentYear', ()=>{
     return new Date().getFullYear();
 });
@@ -15,6 +35,7 @@ hbs.registerHelper('getCurrentYear', ()=>{
 hbs.registerHelper('screamIt', (text)=>{
     return text.toUpperCase();
 })
+
 // setup your route
 app.get('/', (req,res)=>{
     //sending html
